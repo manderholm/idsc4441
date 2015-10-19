d68b100ea84c5c22480e5514dd5c3f7f5a01eaaf
Matthew Anderholm <matthewanderholm@x-10-104-208-87.uofm-secure.wireless.umn.edu>
2015-10-19 11:37:04 -0500

    checkout

--- checkout.php
+++ checkout.php
@@ -9,8 +9,8 @@
 
 <body>
 	<h2 class="jumbotron"> Checkout </h2>
-	<h4> Order Summary: </h4>
 	<div class="container">
+		<h4> Order Summary: </h4>
 		<?php 
 		echo "<div class='row'>";
 		for ($i = 0; $i < (int) $_POST['noTransactions']; $i++ ){
@@ -25,20 +25,22 @@
 			}
 		} 
 
-		echo "<div class='span6'>";
+		echo "<div class='col-md-6'>";
 		echo " <h5> Overall: </h5>";
 		echo "Total number of items: " . $_POST['totalItems'];
 		echo "<br/> Total Price: " . $_POST['totalCost'];
 		echo "</div></div> <br/>";
 
 		?>
+		<h4> Enter Billing Information: </h4>
 		<div class='row'>
-			<div class='span12'>
-				<h4> Payment Options: </h4>
+			<div class='col-md-6'>
+				<h5> Payment Options: </h4>
 				<span> Choose Existing Payment Option: </span>
 				<form method='post' action='complete.php'>
 					<input type="radio" name="payment" value="Visa: ********1111">Visa: ********1111<br/>
 					<input type="radio" name="payment" value="American Express: ********0005">American Express: ********0005<br/>
+					<input type="radio" name="payment" value="PayPal">PayPal<br/>
 					<?php
 						echo "<input type='hidden' name='total' value='".$_POST['totalCost']."'>";
 						echo "<input type='hidden' name='noTransactions' value='". $_POST['noTransactions'] ."'>";
@@ -50,6 +52,24 @@
 
 					?>
 					<button> Add new payment method </button>
+					<input type="submit">
+				</form>
+			</div>
+			<div class='col-md-6'>
+				<h5> Address: </h4>
+				<span> Choose Address: </span>
+				<form method='post' action='complete.php'>
+					<input type="radio" name="payment" value="Visa: ********1111">1015 15th Ave SE, Minneapolis MN<br/>
+					<?php
+						echo "<input type='hidden' name='total' value='".$_POST['totalCost']."'>";
+						echo "<input type='hidden' name='noTransactions' value='". $_POST['noTransactions'] ."'>";
+						for ($i = 0; $i < (int) $_POST['noTransactions']; $i++ ){
+							echo "<input type='hidden' name='retailer".$i."' value='".$_POST['retailer' . $i]."'>";
+							echo "<input type='hidden' name='retailerItemNo".$i."' value='".$_POST['retailerItemNo' . $i]."'>";
+							echo "<input type='hidden' name='retailerPrice".$i."' value='".$_POST['retailerPrice' . $i]."'>";
+						} 
+					?>
+					<button>Add new address</button>
 					<input type="submit">
 				</form>
 			</div>
